df6bf5c0c2077d65b1d8a106771bb730384e60dc
chethana-rao <chethanaprao@alumni.iitm.ac.in>
2025-02-11 15:27:28 +0000

    CSV files export

--- Che_changes/Body_parts/Half_blood_vessel/demo_csv_file.jl
+++ Che_changes/Body_parts/Half_blood_vessel/demo_csv_file.jl
@@ -3,29 +3,36 @@ using Comodo.GLMakie
 using Comodo.GeometryBasics
 using Comodo.Statistics
 using FileIO
+using Printf
 
 
-ind_ins= Cb .== 1 
-indsh = findall(ind_ins)
-Finner = Fbs[indsh]
-Finner,Vinner = separate_vertices(Finner,Vbs)            
-#try isolating Finner and Vinner 
-Ms = GeometryBasics.Mesh(Vinner,Finner)
-#visualizatio
-f = Figure(size=(100,100))
-ax1 = Axis3(f[1, 1],aspect = :data,title="Cylinder")
-hp1 = mesh!(ax1,Ms, shading = FastShading, transparency=true, overdraw=false)
-display(GLMakie.Screen(),f)
+# Finner = Fb[Cb .== 1]
+# # Finner,Vinner = separate_vertices(Finner,Vbs)            
+# #try isolating Finner and Vinner 
+# Ms = GeometryBasics.Mesh(V,Finner)
+# #visualizatio
+# f = Figure(size=(100,100))
+# ax1 = Axis3(f[1, 1],aspect = :data,title="Cylinder")
+# hp1 = mesh!(ax1,Ms, shading = FastShading, transparency=true, overdraw=false)
+# display(GLMakie.Screen(),f)
 
 
-# open("Voutter.csv", "w") do io
-#     for v in Vinner
-#         println(io, "$(v.data[1]),$(v.data[2]),$(v.data[3])")
-#     end
-# end
+open("Nodes_data.csv", "w") do io
+    for v in V
+         @printf(io,"%.16e, %.16e, %.16e\n",v[1],v[2], v[3])
+     #   println(io, "$(v.data[1]),$(v.data[2]),$(v.data[3])")
+    end
+end
 
-# open("Foutter.csv", "w") do io
-#     for f in Finner
-#         println(io, "$(f.data[1]),$(f.data[2]),$(f.data[3])")
-#     end
-# end
+open("Faces_boundary.csv", "w") do io
+    for (i,f) in enumerate(Fb)
+        @printf(io,"%i, %i, %i, %i\n",f[1],f[2],f[3],Cb[i])
+      #  println(io, "$(f.data[1]),$(f.data[2]),$(f.data[3])")
+    end
+end
+open("Elements.csv", "w") do io
+    for (i,e) in enumerate(E)
+        @printf(io,"%i, %i, %i, %i\n",e[1],e[2],e[3],CE[i])
+      #  println(io, "$(f.data[1]),$(f.data[2]),$(f.data[3])")
+    end
+end
